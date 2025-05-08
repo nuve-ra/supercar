298b9fb1243d08efc30e5815024d0d3010491589
nupoo <nuve19ec@cmrit.ac.in>
2025-05-08 19:57:40 +0530

    responsive

--- script.js
+++ script.js
@@ -2,20 +2,47 @@ const links = document.querySelectorAll('.tooltip-link');
 const tooltip = document.getElementById('tooltip');
 
 links.forEach(link => {
+  // Desktop (mouse events)
   link.addEventListener('mouseenter', (e) => {
-    tooltip.style.display = 'block';
-    tooltip.textContent = link.dataset.tooltip;
+    showTooltip(e, link);
   });
 
   link.addEventListener('mousemove', (e) => {
-    tooltip.style.left = e.pageX + 15 + 'px';
-    tooltip.style.top = e.pageY + 15 + 'px';
+    moveTooltip(e);
   });
 
   link.addEventListener('mouseleave', () => {
-    tooltip.style.display = 'none';
+    hideTooltip();
+  });
+
+  // Mobile (touch events)
+  link.addEventListener('touchstart', (e) => {
+    showTooltip(e.touches[0], link);
+  });
+
+  link.addEventListener('touchmove', (e) => {
+    moveTooltip(e.touches[0]);
+  });
+
+  link.addEventListener('touchend', () => {
+    hideTooltip();
   });
 });
+
+function showTooltip(e, link) {
+  tooltip.style.display = 'block';
+  tooltip.textContent = link.dataset.tooltip;
+  moveTooltip(e);
+}
+
+function moveTooltip(e) {
+  tooltip.style.left = e.pageX + 15 + 'px';
+  tooltip.style.top = e.pageY + 15 + 'px';
+}
+
+function hideTooltip() {
+  tooltip.style.display = 'none';
+}
 
 function goTo(page) {
   window.location.href = page;
